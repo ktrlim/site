c332cdfbfb05ff2ca3cfbc109add9e95e1a0515e
Katie <katherinerlim@gmail.com>
2024-07-31 23:18:29 -0400

    finished basics

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menulinks");
+    const icon = document.querySelector(".hamburgericon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+// sets the interval to change the artist photo that is displayed
+// starts the timer for the window
+function startTimer()
+{
+   var interval = setInterval('wink()', 3000);
+}
+
+// changes the artist photo displayed
+function wink()
+{
+    var img = document.getElementById("wink");
+    if (img.src.endsWith("about1.png")) {
+        img.src = "./assets/about2.png";
+    } else {
+        img.src = "./assets/about1.png";
+    }
+}
